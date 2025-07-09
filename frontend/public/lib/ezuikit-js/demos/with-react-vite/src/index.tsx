501035ac2f26bac81c5744bcca12a2dd9d68eb46
opuspocus1 <austa.federico@gmail.com>
2025-07-09 18:23:05 -0300

    Add ezuikit-js as normal files

---
+++ frontend/public/lib/ezuikit-js/demos/with-react-vite/src/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Player from "./Player";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <Player />
+);
